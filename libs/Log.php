750a8f493399d887b8d8f777e4653846269b8c07
kelicom <kelicom@qq.com>
2019-01-09 14:30:09 +0800

    一个简单的日志类

--- libs/Log.php
+++ libs/Log.php
@@ -3,51 +3,29 @@ namespace ank;
 
 class Log
 {
-    private static $_instance = null;
-    private function __construct()
+    private $logPath = '';
+    public function __construct($logPath = '')
     {
+        $logPath = $logPath;
 
     }
-    public static function getInstance()
+    public function write($str, $type = 'log')
     {
-        if (self::$_instance == null) {
-            $_instance = new self;
-        }
-        return $_instance;
-    }
-    public static function write($str, $type = 'log')
-    {
-        if (!App::config('log.start')) {
-            return;
-        }
-        $log_level = App::config('log.level');
-        if ($log_level && !in_array($type, $log_level)) {
-            return;
-        }
-        $log_path = App::config('log.path');
-        if (!$log_path) {
-            $log_path = DATA_PATH . '/logs';
-        }
+
         $cli = '';
         if (IS_CLI) {
             $cli = 'cli_';
         }
-        //这个地方因为路由模块还没有初始化完成可能会进入死循环
-        if (Route::hasInstance()) {
-            $file_path = $log_path . '/' . Route::getInstance()->getModule();
-        } else {
-            $file_path = $log_path . '/unknow';
+        if (!file_exists($this->logPath)) {
+            mkdir($this->logPath, 0777, true);
         }
-        if (!file_exists($file_path)) {
-            mkdir($file_path, 0777, true);
-        }
-        $file_path .= '/' . date('y-m-d') . '_' . $cli . $type . '.log';
+        $filePath = $this->logPath . '/' . date('y-m-d') . '_' . $cli . $type . '.log';
         if (!is_string($str)) {
             //为啦方便查看,中文不转义
             $str = json_encode($str, JSON_UNESCAPED_UNICODE);
         }
         $time   = date('Y-m-d H:i:s : ');
         $logstr = "{$time}{$str}\r\n";
-        file_put_contents($file_path, $logstr, FILE_APPEND);
+        file_put_contents($filePath, $logstr, FILE_APPEND);
     }
 }
