1472ff2c212aae908c983d58dcbc75ec2441f268
kelicom <735579768@qq.com>
2019-10-28 22:01:18 +0800

    f

--- example.php
+++ example.php
@@ -1,7 +1,9 @@
 <?php
 include __dir__ . '/vendor/autoload.php';
 
-$logger = new \ank\Log(['logPath' => __dir__ . '/logs/{Y}{m}/{d}/{type}']);
+$logger = new \ank\Log([
+    'logPath' => __dir__ . '/logs/{Y}{m}/{d}/{type}',
+]);
 $logger->write('test log', 'sql');
 $logger->warning('test log');
 // $logger->header('=================');
